0006e5ea915571dc8336d9d4baad9aba8a7757c8
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-04 17:43:36 +0100

    Navbar inserita

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@
 function App() {
   //Propietà (className/props)
   return (
-    
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Sushi-React</span>
+      </div >
+    </nav>
+
   );
 }
 //Esportare il componente per visualizzarlo a schermo
