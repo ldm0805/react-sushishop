9f6a551017c8c7e7700c17b46307ab6eb4716031
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-04 18:13:11 +0100

    Conversione da function a component per gli elementi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 //Sezione import
+import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Card from './components/card';
 
 
 //Dichiaro la funzione App
-function App() {
-  //Propietà (className/props)
-  return (
-    // Serve il fragment jsx per racchiudere le due compontenti siccome react non accetta due componenti una dopo l'altra
-    <>
-      <Navbar />
-      <div className="container">
-        <h1>Cosa desideri ordinare?</h1>
-        <hr />
-        <div className="row">
-          <Card />
+class App extends Component {
+  render() {
+    //Propietà (className/props)
+    return (
+      // Serve il fragment jsx per racchiudere le due compontenti siccome react non accetta due componenti una dopo l'altra
+      <>
+        <Navbar />
+        <div className="container">
+          <h1>Cosa desideri ordinare?</h1>
+          <hr />
+          <div className="row">
+            <Card />
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 //Esportare il componente per visualizzarlo a schermo
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,21 @@
 //Bisogna trattare le propietà style come se fossero degli oggetti in js.
-
+import React, { Component } from 'react';
 import california from '../images/california.png';
-
-function Card() {
-    return (
-        <div className="col">
-            <div className="card" style={{ width: '18rem', textAlign: 'center' }}>
-                <img className="card-img-top" src={california} alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">California Roll</h5>
-                    <p className="card-text">1.99</p>
-                    <button className="btn btn-outline-danger">Elimina</button>
+class Card extends Component {
+    render() {
+        return (
+            <div className="col" >
+                <div className="card" style={{ width: '18rem', textAlign: 'center' }}>
+                    <img className="card-img-top" src={california} alt="california" />
+                    <div className="card-body">
+                        <h5 className="card-title">California Roll</h5>
+                        <p className="card-text">1.99</p>
+                        <button className="btn btn-outline-danger">Elimina</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Card;
