a3938f2792a1f392a95b55e32e4a05e78038dec1
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-04 17:37:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//Sezione import
+
+
+//Dichiaro la funzione App
+function App() {
+  //Propietà (className/props)
+  return (
+    
+  );
+}
+//Esportare il componente per visualizzarlo a schermo
+export default App;
